4e359ae98bfb28a92966a68eaafdc148ab93a720
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-20 18:00:08 -0800

    add answer variable, add print expression and answer

--- mathGame.py
+++ mathGame.py
@@ -10,7 +10,9 @@ def generate_problem():
   operator = random.choice(OPERATORS)
 
   expr = str(left) + " " + operator + " " + str(right)
-  print(expr)
-  return expr
+  # eval() will evaluate a string as if it were a python expression
+  answer = eval(expr)
+  return expr, answer
 
-generate_problem()
+expr, answer = generate_problem()
+print(expr, answer)
