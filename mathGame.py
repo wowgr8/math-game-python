e585b23f044aeacdf52e4bce9c2815eb14cdb54b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-20 18:17:36 -0800

    add timer variables, add start input

--- mathGame.py
+++ mathGame.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
@@ -15,9 +16,22 @@ def generate_problem():
   answer = eval(expr)
   return expr, answer
 
+wrong = 0
+input("Press enter to start!")
+print("-----------------------")
+
+start_time = time.time()
+
 for i in range(TOTAL_PROBLEMS):
   expr, answer = generate_problem()
   while True:
     guess = input("Problem # " + str(i + 1) + ": " + expr + " = ")
     if guess == str(answer):
       break
+    wrong += 1
+
+end_time = time.time()
+total_time = end_time - start_time
+
+print("---------------------")
+print("Nice work! You finished in", total_time, "seconds!")
